e013f5f425327468bff64dfa27fd97ef84a2c969
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-07-24 18:01:19 +0430

    Todoey Your very own ToDo List App & Todoey Your very own ToDo List App

---
+++ android/app/src/main/kotlin/tech/craftcoding/todoey/MainActivity.kt
@@ -0,0 +1,5 @@
+package tech.craftcoding.todoey
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,72 @@
+import "package:flutter/material.dart";
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+        ),
+        elevation: 5,
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {},
+        child: Icon(Icons.add, color: Colors.white, size: 50.0),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              top: 120.0,
+              bottom: 30.0,
+              left: 50.0,
+              right: 30.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 30.0,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.lightBlueAccent,
+                    size: 40.0,
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  "Todoey",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  "12 Tasks",
+                  style: TextStyle(color: Colors.white, fontSize: 15.0),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
