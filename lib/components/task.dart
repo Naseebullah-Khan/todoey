1dda7ec2f906da21e52f591e73c969ff226b2698
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-07-31 16:49:55 +0430

    What is State and How do we Manage it & How do Callbacks work in Dart

--- lib/components/task.dart
+++ lib/components/task.dart
@@ -1,15 +1,56 @@
 import "package:flutter/material.dart";
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   const Task({super.key, required this.taskText});
 
   final String taskText;
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(taskText),
-      trailing: Checkbox(value: false, onChanged: (bool? value) {}),
+      title: Text(
+        widget.taskText,
+        style: TextStyle(
+          decoration: isChecked
+              ? TextDecoration.lineThrough
+              : TextDecoration.none,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        isCompleted: isChecked,
+        onChanged: (bool? value) {
+          setState(() {
+            isChecked = value!;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({
+    super.key,
+    required this.isCompleted,
+    required this.onChanged,
+  });
+
+  final bool isCompleted;
+  final void Function(bool?)? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isCompleted,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: onChanged,
     );
   }
 }
